f1941a6f85bc8854db143da26a5bf1da52f16627
Glitch-tiger <lofa45@gmail.com>
2026-01-26 21:52:32 +0300

    Добавлена конфигурация для Render

--- pb.js
+++ pb.js
@@ -1,3 +1,11 @@
 ﻿import PocketBase from "pocketbase";
-const pb = new PocketBase("http://127.0.0.1:8090");
+
+// На Render оба сервиса на одном домене, но разных портах
+// PocketBase на порту 8090, React на 3000
+const pbUrl = window.location.hostname === 'localhost' 
+  ? "http://localhost:8090"
+  : `${window.location.origin.replace(/:\d+/, '')}:8090`;
+
+const pb = new PocketBase(pbUrl);
 export default pb;
+
