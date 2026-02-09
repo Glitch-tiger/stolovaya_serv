cf2cdee175e9913703429229b6f6a036f9e4eb8e
Glitch-tiger <lofa45@gmail.com>
2026-02-09 16:18:55 +0300

    TestMain3

--- db/utils.js
+++
@@ -1,6 +0,0 @@
-﻿// раньше это было pb.js. Добавляйте сюда вспомогательные функции.
-// но хуки для pb-js-движка лучше в pb_hooks. https://pocketbase.io/docs/js-overview/
-import PocketBase from 'pocketbase';
-// это у вас обвязка для PocketBase, чтобы в каждой странице не писать new PocketBase()
-const pb = new PocketBase(import.meta.env.VITE_PB_URL);
-export default pb;
