901804f80310741aa9b920e7ac8e10b2ed3928e2
BobRovi4 <internemir@gmail.com>
2026-02-13 00:29:56 +0300

    названия

--- src/index.jsx
+++ src/index.jsx
@@ -1,21 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter } from 'react-router-dom';
-
-
+import Приложение from './Приложение';  // ИМПОРТ С РУССКИМ НАЗВАНИЕМ!
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Приложение />
     </BrowserRouter>
   </React.StrictMode>
 );
-
-
-reportWebVitals();
